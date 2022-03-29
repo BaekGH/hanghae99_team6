7cd22823fe173bc88cbb15e1b7f0c13e79956b44
BaekGH <86420052+BaekGH@users.noreply.github.com>
2022-03-29 22:36:30 +0900

    bgh

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print('testing')
+from pymongo import MongoClient
+from flask import Flask, jsonify, render_template
+from subprocess import call
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+socket_io = SocketIO(app,cors_allowed_origins='*')
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+
+@socket_io.on("message")
+def request(message):
+    print("message : "+ message)
+    to_client = dict()
+    if message == 'new_connect':
+        to_client['message'] = "새로운 유저가 난입하였다!!"
+        to_client['type'] = 'connect'
+    else:
+        to_client['message'] = message
+        to_client['type'] = 'normal'
+    # emit("response", {'data': message['data'], 'username': session['username']}, broadcast=True)
+    send(to_client, broadcast=True)
+
+if __name__ == '__main__':
+    socket_io.run(app,host='0.0.0.0', debug=True, port=5000)
